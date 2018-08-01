18e2e627a0a201d63c48a4299a540c6070118d70
DESKTOP-AQQD4TP\User <lvbao.cntt.sgu@gmail.com>
2018-08-01 11:17:16 +0700

    chang tu regits

--- js/custom.js
+++ js/custom.js
@@ -10,63 +10,65 @@ $(function(){
 });
 $(document).ready(function() {
     $('.btn-dangki').on('click',function(){
-        $('#form-dangki').validate({
-            rules:{
-                txt_name :{
-                    required : true
-                },
-                rd_sex :{
-                    required:true
-                },
-                txt_address:{
-                    required:true
-                },
-                txt_birthday:{
-                    required:true
-                },
-                txt_bhyt:{
-                    required:true
-                },
-                txt_cmt:{
-                    required:true,
-                    number: true,
-                    maxlength : 9,
-                    minlength: 9
-                },
-                txt_cmt:{
-                    required:true,
-                    number: true,
-                    maxlength : 9,
-                    minlength: 9
-                },
-                txt_dantoc:{
-                    required:true,
-                },
-                txt_email:{
-                    email:true
-                },
-                txt_username :{
-                    required:true,
-                },
-                txt_password:{
-                    required:true,
-                    minlength:6
-                },
-            },
-            messages:{
 
-            },
-            errorClass: "label label-danger",
-            highlight: function (element, errorClass, validClass) {
-                return false;
-            },
-            unhighlight: function (element, errorClass, validClass) {
-                return false;
-            }
-        });
-        $valid = false;
-        if($valid){
-            $('#form-dangki').submit();
-        }
+        $('#form-dangki').submit();
+        // $('#form-dangki').validate({
+        //     rules:{
+        //         txt_name :{
+        //             required : true
+        //         },
+        //         rd_sex :{
+        //             required:true
+        //         },
+        //         txt_address:{
+        //             required:true
+        //         },
+        //         txt_birthday:{
+        //             required:true
+        //         },
+        //         txt_bhyt:{
+        //             required:true
+        //         },
+        //         txt_cmt:{
+        //             required:true,
+        //             number: true,
+        //             maxlength : 9,
+        //             minlength: 9
+        //         },
+        //         txt_cmt:{
+        //             required:true,
+        //             number: true,
+        //             maxlength : 9,
+        //             minlength: 9
+        //         },
+        //         txt_dantoc:{
+        //             required:true,
+        //         },
+        //         txt_email:{
+        //             email:true
+        //         },
+        //         txt_username :{
+        //             required:true,
+        //         },
+        //         txt_password:{
+        //             required:true,
+        //             minlength:6
+        //         },
+        //     },
+        //     messages:{
+
+        //     },
+        //     errorClass: "label label-danger",
+        //     highlight: function (element, errorClass, validClass) {
+        //         return false;
+        //     },
+        //     unhighlight: function (element, errorClass, validClass) {
+        //         return false;
+        //     }
+        // });
+        // $valid = false;
+        // if($valid){
+        //     $('#form-dangki').submit();
+        // }
    });
 });
